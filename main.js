225697553307080f8c49ae63a34ec3ffe41485f0
jein4433 <jein4433@gmail.com>
2024-07-09 01:41:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let news = [];
 const getLatestNews = async () => {
 
     let url = `https://news-site-e2dedb.netlify.app/top-headlines`
+    
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;
